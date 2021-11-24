35f6ff8fdff7de5fdc05fcf2cadb30dd1a0c8a92
Madelief Verburg <BFDmacbookpro2015@MacBook-Pro-van-Madelief.local>
2021-11-24 13:45:57 +0100

    block values optimized

--- sudoku.py
+++ sudoku.py
@@ -7,8 +7,10 @@ class Sudoku:
 
     def __init__(self, puzzle: Iterable[Iterable]):
         self._grid: list = []
-        self._grid_collumn: list = []
+        self._grid_column: list = []
+        self._grid_block: list = []
 
+        # Filling the grid with values in puzzle
         for puzzle_row in puzzle:
             row = []
 
@@ -17,22 +19,47 @@ class Sudoku:
 
             self._grid.append(row)
 
+        # Filling the columns grid by using values in grid and reversing them
         for i in range(9):
-            collumn = []
+            column = []
             for j in range(9):
-                collumn.append(self._grid[j][i])
+                column.append(self._grid[j][i])
 
-            self._grid_collumn.append(collumn)
+            self._grid_column.append(column)
+
+        # Filling block grid using the grid
+        for i in range(9):
+            block = []
+            x_start = (i % 3) * 3
+            y_start = (i // 3) * 3
+            for x in range(x_start, x_start + 3):
+                for y in range(y_start, y_start + 3):
+                    block.append(self._grid[y][x])
+            self._grid_block.append(block)
 
     def place(self, value: int, x: int, y: int) -> None:
         """Place value at x,y."""
         self._grid[y][x] = int(value)
-        self._grid_collumn[x][y] = int(value)
+
+        # Changing value at column grid
+        self._grid_column[x][y] = int(value)
+
+        # Changing value in block grid
+        block_index = (y // 3) * 3 + x // 3
+        block_list_index = (x % 3) * 3 + y % 3
+        self._grid_block[block_index][block_list_index] = int(value)
 
     def unplace(self, x: int, y: int) -> None:
         """Remove (unplace) a number at x,y."""
         self._grid[y][x] = 0
-        self._grid_collumn[x][y] = 0
+
+        # Changing value at column grid to 0
+        self._grid_column[x][y] = 0
+
+        # Changing value at block grid to 0
+        block_index = (y // 3) * 3 + x // 3
+        block_list_index = (x % 3) * 3 + y % 3
+        self._grid_block[block_index][block_list_index] = 0
 
     def value_at(self, x: int, y: int) -> int:
         """Returns the value at x,y."""
@@ -47,7 +74,7 @@ class Sudoku:
         # Get the index of the block based from x,y
         block_index = (y // 3) * 3 + x // 3
 
-        # Remove all values from row collum and block
+        # Remove all values from row, column and block from options
         options = options - set(self.row_values(y)) - set(self.column_values(x)) - set(self.block_values(block_index))
 
         return options
@@ -73,7 +100,7 @@ class Sudoku:
 
     def column_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th column."""
-        return self._grid_collumn[i]
+        return self._grid_column[i]
 
     def block_values(self, i: int) -> Iterable[int]:
         """
@@ -83,16 +110,7 @@ class Sudoku:
         3 4 5
         6 7 8
         """
-        values = []
-
-        x_start = (i % 3) * 3
-        y_start = (i // 3) * 3
-
-        for x in range(x_start, x_start + 3):
-            for y in range(y_start, y_start + 3):
-                values.append(self.value_at(x, y))
-
-        return values
+        return self._grid_block[i]
 
     def is_solved(self) -> bool:
         """
